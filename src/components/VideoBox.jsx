91da021a04dcdff7569e09c5909af3f995450703
Klubuntu <Kb.klubuntu@wp.pl>
2025-05-11 03:40:49 +0200

    Redesigned container of video player

--- src/components/VideoBox.jsx
+++ src/components/VideoBox.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import {Card, CardHeader, CardBody, Image} from "@heroui/react";
+import { Card, CardHeader, CardBody } from "@heroui/react";
 import VideoPlayer from "./VideoPlayer";
 
-export default function (props) {
+export default function VideoBox({ name, src }) {
   return (
-    <Card className="py-4 rounded-lg mx-[20vw] max-md:mx-[10vw] max-lg:mx-[5vw] bigger:max-w-[1500px] bigger:mx-auto bigger:block">
-      <CardHeader className="pb-2 pt-2 px-1 flex-col items-center ">
-        <h4 className="font-bold text-xl">{props.name}</h4>
+    <Card className="min-w-96 w-full max-w-screen-lg mx-auto p-4 flex justify-center items-center bg-zinc-900 border border-zinc-700 rounded-xl shadow-lg">
+      <CardHeader className="w-auto pb-2 pt-2 px-1 text-center">
+        <h4 className="font-bold text-2xl text-white text-center">{name}</h4>
       </CardHeader>
-      <CardBody className="overflow-visible py-2 items-center">
-        <VideoPlayer src={props.src} width="51.5vw"/>
+      <CardBody className="overflow-visible pt-4 flex justify-center items-center">
+        <VideoPlayer src={src} />
       </CardBody>
     </Card>
   );
