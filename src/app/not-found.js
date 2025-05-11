08ea653a86ce32c37ba05c9159cfaa1d1bd102bd
Klubuntu <Kb.klubuntu@wp.pl>
2025-05-11 12:01:21 +0200

    Improved 404 page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,25 +1,33 @@
 'use client';
 
 import React from 'react';
-import { HeroUIProvider } from "@heroui/react";
+import { HeroUIProvider } from '@heroui/react';
 import Link from 'next/link';
-import getLangData from '@/components/server/getLangData';
-// import Navbar from '@/components/Navbar';
+import Head from 'next/head';
 
+export default function NotFound() {
+  const text404 = "Sorry, the page you're looking for doesn't exist.";
+  const returnText = "Go back home";
 
-export default async function NotFound() {
-  const data = await getLangData();
-  const text404 = data.pages["404"]
-  const return_text = data.pages.return_page;
   return (
-    <HeroUIProvider>
-        <div className="App">
-            {/* <Navbar /> */}
-            <div className="text-center text-l mt-10">
-              <Link href="/">{return_text}</Link>
-              <h2 className='text-center text-xl mt-10 mx-5 sm:mx-4 lg:mx-0'>{text404}</h2>
-            </div>
-        </div>
-    </HeroUIProvider>
-  )
+    <>
+      <Head key="not-found-title">
+        <title>iTVT Hub - 404 Page not found</title>
+      </Head>
+      <HeroUIProvider>
+        <main className="flex flex-col items-center justify-center min-h-screen px-4">
+          <section className="text-center">
+            <h1 className="text-6xl font-bold text-accent">404</h1>
+            <p className="mt-4 text-xl text-red-700">{text404}</p>
+            <Link
+              href="/"
+              className="inline-block mt-6 px-6 py-3 bg-zinc-600 text-white rounded hover:bg-zinc-700 transition"
+            >
+              {returnText}
+            </Link>
+          </section>
+        </main>
+      </HeroUIProvider>
+    </>
+  );
 }
