11161a1d804de1149fcad5c1f88d15ef2034a15a
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2026-01-15 09:53:49 +0100

    Updated test on First Loading Popup
    Updated the text in the Popup component for clarity.

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -5,7 +5,7 @@ const Popup = ({ onClose }) => {
     <div id="popup" className="fixed inset-0 bg-black bg-opacity-90 flex justify-center items-center z-50">
       <div className="bg-black p-8 rounded shadow-lg text-center">
         <h2 className="text-2xl font-bold mb-4">Weryfikacja uprawnień</h2>
-        <p className="mb-4">Witaj, wchodząc na stronę wyrażasz zgodę na zapisywanie plików cookie (zwanych ciastkami) w twojej przeglądarce<br></br>Zgadzasz się również na monitorowanie stanu aktywności twojego przebywania na stronie oraz wyrażasz zgodę na zbieranie danych na temat twojej przeglądarki i systemu w celu optymalizacji lepszego działania naszej strony.</p>
+        <p className="mb-4">Witaj, wchodząc na stronę wyrażasz zgodę na zapisywanie plików cookie (zwanych ciastkami) w twojej przeglądarce<br></br>Zgadzasz się również na monitorowanie twojego stanu aktywności przebywania na tej stronie oraz wyrażasz zgodę na zbieranie danych na temat twojej przeglądarki i systemu w celu optymalizacji lepszego działania naszej strony.</p>
         <button onClick={onClose} className="px-4 py-2 bg-blue-700 text-white rounded">
           Rozumiem
         </button>
@@ -13,5 +13,6 @@ const Popup = ({ onClose }) => {
     </div>
   );
 };
+
 
 export default Popup;
